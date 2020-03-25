a8b243d424d1881cdd5de02ccc7b561d49b1854b
Erichollman96 <erichollman96@gmail.com>
2020-03-25 19:34:43 -0400

    Update Settings.Designer.vb

--- Settings.Designer.vb
+++ Settings.Designer.vb
@@ -57,30 +57,6 @@ Namespace My
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.DefaultSettingValueAttribute("")>  _
-        Public Property Salespeople() As String
-            Get
-                Return CType(Me("Salespeople"),String)
-            End Get
-            Set
-                Me("Salespeople") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
-        Public Property Product() As String
-            Get
-                Return CType(Me("Product"),String)
-            End Get
-            Set
-                Me("Product") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
         Public Property Client() As String
             Get
                 Return CType(Me("Client"),String)
@@ -149,6 +125,30 @@ Namespace My
                 Me("ServerName") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property Product() As String
+            Get
+                Return CType(Me("Product"),String)
+            End Get
+            Set
+                Me("Product") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property Salespeople() As String
+            Get
+                Return CType(Me("Salespeople"),String)
+            End Get
+            Set
+                Me("Salespeople") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
@@ -160,9 +160,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.WindowsApp1.My.MySettings
+        Friend ReadOnly Property Settings() As Global.WinDryRunApp.My.MySettings
             Get
-                Return Global.WindowsApp1.My.MySettings.Default
+                Return Global.WinDryRunApp.My.MySettings.Default
             End Get
         End Property
     End Module
