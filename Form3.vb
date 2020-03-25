9d378b6eb5617c20e58c95e3f5a28f1ff9ce90e4
Erichollman96 <erichollman96@gmail.com>
2020-03-25 19:15:57 -0400

    Update Form3.vb

--- Form3.vb
+++ Form3.vb
@@ -10,29 +10,45 @@ Public Class Form3
         connection.Open()
         Dim queryTable As New DataTable() ' Declares a data table and SQL command
         Dim myCmd As New MySqlCommand()
+        Dim Command As String
         myCmd.Connection = connection
         If String.IsNullOrEmpty(Cbox_Client2.Text) And String.IsNullOrEmpty(Cbox_SalesPerson2.Text) And String.IsNullOrEmpty(Cbox_Product2.Text) Then ' Checks if specified fields have values inserted into them. If no values are present, the fields are not used as conditions in the SQL statement
-            myCmd.CommandText = " SELECT * FROM test.orders"
+            Command = " SELECT * FROM test.orders"
         ElseIf String.IsNullOrEmpty(Cbox_Client2.Text) And String.IsNullOrEmpty(Cbox_SalesPerson2.Text) Then
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE Product= '" & Cbox_Product2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE Product= '" & Cbox_Product2.Text & "' "
         ElseIf String.IsNullOrEmpty(Cbox_Product2.Text) And String.IsNullOrEmpty(Cbox_SalesPerson2.Text) Then
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE Client= '" & Cbox_Client2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE Client= '" & Cbox_Client2.Text & "' "
         ElseIf String.IsNullOrEmpty(Cbox_Product2.Text) And String.IsNullOrEmpty(Cbox_Client2.Text) Then
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' "
         ElseIf String.IsNullOrEmpty(Cbox_Product2.Text) Then
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' AND Client= '" & Cbox_Client2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' AND Client= '" & Cbox_Client2.Text & "' "
         ElseIf String.IsNullOrEmpty(Cbox_Client2.Text) Then
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' AND Product= '" & Cbox_Product2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' AND Product= '" & Cbox_Product2.Text & "' "
         ElseIf String.IsNullOrEmpty(Cbox_SalesPerson2.Text) Then
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE Product= '" & Cbox_Product2.Text & "' AND Client= '" & Cbox_Client2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE Product= '" & Cbox_Product2.Text & "' AND Client= '" & Cbox_Client2.Text & "' "
         Else
-            myCmd.CommandText = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' AND Product= '" & Cbox_Product2.Text & "' AND Client= '" & Cbox_Client2.Text & "' "
+            Command = " SELECT * FROM test.orders WHERE SalesPerson= '" & Cbox_SalesPerson2.Text & "' AND Product= '" & Cbox_Product2.Text & "' AND Client= '" & Cbox_Client2.Text & "' "
         End If
 
+        myCmd.CommandText = Command
         Dim myAdapater1 As New MySqlDataAdapter(myCmd) 'Fills the data grid with the results of one of the above SQL commands
         myAdapater1.Fill(queryTable)
         Datagrid_SQL.DataSource = queryTable
         connection.Close()
+
+
+
+        connection.Open()
+        Dim command2 As String = Command.Replace("*", "COUNT(ID)")
+        Dim cmd3 = New MySqlCommand(command2, connection)
+        Dim totalcount As Int16 = cmd3.ExecuteScalar
+        Dim command3 As String = "select count(ID) from test.orders"
+        Dim cmd4 = New MySqlCommand(command3, connection)
+        Dim matchingrecords As Int16 = cmd4.ExecuteScalar
+        LblSqlResults.Text = totalcount & " out of " & matchingrecords & " records showing"
+        connection.Close()
+
+
     End Sub
 
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
